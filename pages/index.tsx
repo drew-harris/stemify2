ac9eef678ccba9867fa0d7179fa181cce0330d2d
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-03-04 14:31:22 -0600

    Color pallette 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="mx-auto text-center text-[28px] mt-32">Stemify 2</div>
-  )
-}
+    <>
+      <div className="mx-auto text-center text-neutral-800 font-bold text-[28px] mt-32">
+        Stemify 2
+      </div>
+      <div className="flex flex-row   justify-center ">
+        <div className="w-24 m-3 h-24 bg-tan-50 rounded-b-md"></div>
+        <div className="w-24 m-3 h-24 bg-tan-100"></div>
+        <div className="w-24 m-3 h-24 bg-tan-200"></div>
+        <div className="w-24 m-3 h-24 bg-tan-250"></div>
+        <div className="w-24 m-3 h-24 bg-tan-300"></div>
+        <div className="w-24 m-3 h-24 bg-tan-400"></div>
+        <div className="w-24 m-3 h-24 bg-tan-500"></div>
+        <div className="w-24 m-3 h-24 bg-tan-700"></div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      tan: {
+        50: "#FFFCEF",
+        100: "#EBEAE6",
+        200: "#DAD6CD",
+        250: "#B1A99E",
+        300: "#D3B48E",
+        400: "#87735B",
+        500: "#544738",
+        700: "#130F0D",
+      },
+    },
   },
   plugins: [],
 };
