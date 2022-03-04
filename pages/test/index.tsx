6bf0ef296f73fb1f81d85ae14a2c3dede37e4e3f
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-03-03 22:57:08 -0600

    Test Page

---
+++ pages/test/index.tsx
@@ -0,0 +1,45 @@
+import { Prisma } from "@prisma/client"
+import { useEffect } from "react"
+import { getPrismaPool } from "../../server_helpers/prismaPool"
+
+export default function Home({songs}: any) {
+  useEffect(() => {
+    console.log(songs)
+  }, [songs])
+  const songComponents = songs.map((song:any)=> (
+    <div key={song.id}>
+      <h2>{song.title}</h2>
+      <pre>{JSON.stringify(song, null, 2)}</pre>
+    </div>
+  ))
+  return (
+    <>
+    <div>Stemify 2 Soonr</div>
+    {songComponents}
+    </>
+  )
+}
+
+export async function getServerSideProps(context: any) {
+  const prisma = getPrismaPool();
+  const songs = await prisma.song.findMany({
+    where: {
+      complete: true
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    select: {
+      title: true,
+      metadata: true,
+    }
+  })
+  console.log(songs)
+
+  return {
+    props: {
+      songs
+    }
+  }
+  
+}
