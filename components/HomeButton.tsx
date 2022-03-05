d65901af3a04a4cc3fd01241941da9703a6bc86a
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-03-05 16:09:04 -0600

    Fix for refresh on queu

--- components/HomeButton.tsx
+++ components/HomeButton.tsx
@@ -4,7 +4,7 @@ import { faHouse } from "@fortawesome/free-solid-svg-icons";
 export default function HomeButton() {
   return (
     <Link href="/">
-      <div className="m-5 cursor-pointer">
+      <div className="m-5 overflow-hidden cursor-pointer max-w-9 max-h-9">
         <FontAwesomeIcon size="lg" icon={faHouse} />
       </div>
     </Link>
--- pages/library/index.tsx
+++ pages/library/index.tsx
@@ -20,7 +20,6 @@ export default function Home({ songs }: any) {
 
       <div className="grid items-stretch gap-4 p-3 lg:grid-cols-3 sm:grid-cols-2 justify-items-stretch sm:p-9 ">
         {songComponents}
-        {songComponents}
       </div>
     </>
   );
