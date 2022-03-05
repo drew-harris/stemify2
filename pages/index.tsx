d060c05ea0b20e7c0b4a81f7b513d4f8db281687
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-03-04 22:16:54 -0600

    Responsive home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <>
-      <div className="mx-auto text-center text-neutral-800 font-bold text-[28px] mt-32">
+      <div className="mx-auto mb-4 text-center text-neutral-800 font-bold text-[28px] mt-32">
         Stemify 2
       </div>
-      <div className="flex flex-row justify-center ">
+      <div className="flex flex-col sm:flex-row mx-auto items-center justify-center mb-8">
         <div className="w-24 h-24 bg-tan-50 rounded-b-md"></div>
         <div className="w-24 h-24 bg-tan-100"></div>
         <div className="w-24 h-24 bg-tan-200"></div>
