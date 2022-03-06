368e6b9bc1dc06834de2654b1649f58282088775
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-03-06 00:04:51 -0600

    Reopen + add countdown delay

--- pages/closed.tsx
+++ pages/closed.tsx
@@ -4,7 +4,7 @@ export default function Closed() {
   return (
     <div className="grid h-screen text-2xl place-items-center">
       <Countdown
-        date={config.releaseDate}
+        date={config.releaseDate + 3000}
         onComplete={() => location.reload()}
       />
     </div>
