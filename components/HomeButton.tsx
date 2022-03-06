6493673562ce6f6e66daef0bb6b7eb8bccf6e918
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-03-05 21:36:53 -0600

    Add fa script to home button

--- components/HomeButton.tsx
+++ components/HomeButton.tsx
@@ -1,17 +1,28 @@
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import Script from "next/script";
+import Head from "next/head";
 export default function HomeButton() {
   return (
-    <Link href="/">
-      <div className="m-5 overflow-hidden cursor-pointer max-w-9 max-h-9">
-        <FontAwesomeIcon
-          fontSize={10}
-          size="lg"
-          className="overflow-hidden w-30 h-30"
-          icon={faHouse}
-        />
-      </div>
-    </Link>
+    <>
+      <Head>
+        <link
+          href="https://use.fontawesome.com/releases/vVERSION/css/svg-with-js.css"
+          rel="stylesheet"
+        ></link>
+      </Head>
+
+      <Link href="/">
+        <div className="m-5 overflow-hidden cursor-pointer max-w-9 max-h-9">
+          <FontAwesomeIcon
+            fontSize={10}
+            size="lg"
+            className="overflow-hidden w-30 h-30"
+            icon={faHouse}
+          />
+        </div>
+      </Link>
+    </>
   );
 }
