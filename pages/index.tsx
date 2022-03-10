ee9294793cd748e9d2d5e3f8ebbfd9664ad45636
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-03-10 09:00:01 -0600

    Add WIP disclaimer

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ const Home: NextPage = () => {
       <div className="mx-auto mb-4 text-center text-neutral-800 font-bold  text-[28px] mt-32">
         STEMIFY 2
       </div>
+      <div className="mx-auto  text-center text-tan-500 font-bold  text-[28px] ">
+        (WORK IN PROGRESS)
+      </div>
       <div className="flex flex-col items-center justify-center gap-4 pt-8 mx-auto mb-10">
         <Link href="/submit">
           <a className="mb-8 text-xl font-bold underline text-neutral-800 sm:mb-0">
