cf0e2d133b4d7a140ab4cc03c5db878e7939ed58
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-03-14 19:42:51 -0500

    Show songs on album page :|

--- pages/library/album/[id].tsx
+++ pages/library/album/[id].tsx
@@ -2,10 +2,14 @@ import Head from "next/head";
 import { ReactElement, useEffect } from "react";
 import HomeButton from "../../../components/HomeButton";
 import LibraryLayout from "../../../components/layouts/LibraryLayout";
-import BigSong from "../../../components/Songs/BigSong";
+import Song from "../../../components/Songs/Song";
 import { getPrismaPool } from "../../../server_helpers/prismaPool";
 
-function SingleSong({ album }: any) {
+function Album({ album }: any) {
+  const songs = album?.songs;
+  const songComponents = songs?.map((song: any) => (
+    <Song data={song} key={song.id} />
+  ));
   useEffect(() => {
     console.log(album);
   }, [album]);
@@ -14,21 +18,24 @@ function SingleSong({ album }: any) {
       <Head>
         <title>{album?.title || "STEMIFY"}</title>
       </Head>
-      {album && (album.songs[0].title || "")}
+      {songComponents}
     </>
   );
 }
 
-SingleSong.getLayout = function getLayout(page: ReactElement) {
+Album.getLayout = function getLayout(page: ReactElement) {
   console.log("layout loaded");
+  if (!page.props.album) {
+    return <LibraryLayout>{page}</LibraryLayout>;
+  }
   return (
     <LibraryLayout>
-      <SingleSong {...page.props} />
+      <Album {...page.props} />
     </LibraryLayout>
   );
 };
 
-export default SingleSong;
+export default Album;
 
 export async function getStaticProps({ req, res, params }: any) {
   const id = params.id;
@@ -41,7 +48,20 @@ export async function getStaticProps({ req, res, params }: any) {
       id: true,
       title: true,
       artist: true,
-      songs: true,
+      songs: {
+        include: {
+          album: {
+            select: {
+              image: true,
+            },
+          },
+          artist: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
     },
   });
   console.log(album);
