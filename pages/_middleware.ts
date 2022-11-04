c4324e4b2f7a7e403ee73e81e286c63cbf0de829
Drew Harris <dsharris10@gmail.com>
2022-11-04 15:27:13 -0500

    Remove middleware

--- pages/_middleware.ts
+++
@@ -1,9 +0,0 @@
-import type { NextFetchEvent, NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-import config from "../config";
-
-export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  // Before march 7th
-
-  return NextResponse.next();
-}
